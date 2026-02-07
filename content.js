6d8ef410865cd8ac087a8134f563921b0bb4361e
Alex-L464 <alegandro.lopez.h@gmail.com>
2026-02-07 14:00:47 -0600

    chore: update content and popup

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
+const button = document.createElement('button');
+button.type = 'button';
+button.innerText = 'Add to Cart';
+button.style.position = 'fixed';
+button.style.bottom = '20px';
+button.style.right = '20px';
+button.style.padding = '10px 20px';
+button.style.backgroundColor = '#007bff';
+document.body.appendChild(button);
 
