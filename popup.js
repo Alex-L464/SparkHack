2266d48b18e3e90a9d3b46f8e2db3d3d2f0d205c
Alex-L464 <alegandro.lopez.h@gmail.com>
2026-02-07 14:10:55 -0600

    chore: update manifest and popup js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-
-
-
-
-
-document.getElementById('btn').addEventListener('click', () => {
-  alert('You clicked the extension shell!');
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.local.get(['trackedList'], (result) => {
+        const list = result.trackedList || [];
+        const ul = document.getElementById('list');
+        ul.innerHTML = list.map(item => `<li>${item}</li>`).join('');
+    });
 });
+
